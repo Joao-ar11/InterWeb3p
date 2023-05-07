b3d8e4fa8bf34edcc5b067b0c572cef7ba96842e
João_A.11 <jarlpereira29@gmail.com>
2023-05-07 19:20:39 -0300

    Mudar banco novamente

--- index.php
+++ index.php
@@ -1,26 +1,11 @@
 <?php 
 session_start();
 
-
-// Conecta ao banco de dados
-$servidor = "localhost"; // nome do servidor MySQL
-$usuario_bd = "root"; // nome do usuário do banco de dados
-$senha_bd = "root"; // senha do banco de dados
-$nome_bd = "interbd"; // nome do banco de dados
-$conexao = mysqli_connect($servidor, $usuario_bd, $senha_bd, $nome_bd);
-
-//Banco remoto
-//$DB_HOST = $_ENV["DB_HOST"];
-//$DB_USER = $_ENV["DB_USER"];
-//$DB_PASSWORD = $_ENV["DB_PASSWORD"];
-//$DB_NAME = $_ENV["DB_NAME"];
-//$DB_PORT = $_ENV["DB_PORT"];
-
-//$conexao = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
+include './php/conn.php';
 
 $mensagem_erro = "";
 // Verifica se houve erro na conexão
-if (!$conexao) {
+if (!$conn) {
     die("Erro de conexão: " . mysqli_connect_error());
 }
 
@@ -44,7 +29,7 @@ $consulta = "SELECT id, senha, funcao FROM cadastro WHERE email = '$username'";
 
 
 // Executa a consulta SQL
-$resultado = $conexao->query($consulta);
+$resultado = $conn->query($consulta);
 // Verifica se a consulta retornou um resultado
 while ($senha_crip = $resultado->fetch_assoc()){
     if (password_verify($password, $senha_crip['senha'])) {
